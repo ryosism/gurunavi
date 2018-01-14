62524abce1f398f33a666c0bfd70a587491873e7
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2018-01-14 15:25:41 +0900

    Initial Commit

---
+++ gurunaviTests/gurunaviTests.swift
@@ -0,0 +1,36 @@
+//
+//  gurunaviTests.swift
+//  gurunaviTests
+//
+//  Created by 祖父江亮 on 2018/01/14.
+//  Copyright © 2018年 Ryo Sobue. All rights reserved.
+//
+
+import XCTest
+@testable import gurunavi
+
+class gurunaviTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
