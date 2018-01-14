52fa6762c07f2ee1ef74fd2a463d6782e7c4450b
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2018-01-15 00:19:28 +0900

    リクエストに使うクラスを定義、ここで全部データ紐つけて保存したいな

---
+++ gurunavi/DataRequest.swift
@@ -0,0 +1,13 @@
+//
+//  DataRequest.swift
+//  gurunavi
+//
+//  Created by 祖父江亮 on 2018/01/15.
+//  Copyright © 2018年 Ryo Sobue. All rights reserved.
+//
+
+import UIKit
+
+class DataRequest {
+    
+}
