975a720f3888441023d50c21220f9ef589017918
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2018-01-15 01:01:18 +0900

    TableViewControllerの動作確認、とりあえずダミーデータで

--- gurunavi/ResultTableViewController.swift
+++ gurunavi/ResultTableViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ResultTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
+    @IBOutlet weak var resultTable: UITableView!
+    
+    var dummyData:[String] = ["店舗名1","店舗名2","店舗名3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,11 +20,19 @@ class ResultTableViewController: UIViewController, UITableViewDelegate, UITableV
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return dummyData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell =  resultTable.dequeueReusableCell(withIdentifier: "Cell") as! CustomTableViewCell
+        
+        cell.tenpoText.text = dummyData[indexPath.row]
+        
+        let dinnerpngPath = Bundle.main.path(forResource: "dinner", ofType: "png")
+        let dinnerImage = UIImage(contentsOfFile: dinnerpngPath!)
+        cell.topImage.image = dinnerImage
+        
+        return cell
     }
     
     override func didReceiveMemoryWarning() {
