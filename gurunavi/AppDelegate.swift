128dc19661385cbf3c98cfc741a9c0398489960f
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2018-01-15 11:33:56 +0900

    APIで取得して必要な情報を保存(APIKey fixed!)

--- gurunavi/AppDelegate.swift
+++ gurunavi/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var searchResult = [Dictionary<String, Any>]()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
--- gurunavi/DataRequest.swift
+++ gurunavi/DataRequest.swift
@@ -7,7 +7,48 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class DataRequest {
+    let delegate = UIApplication.shared.delegate as! AppDelegate
     
+    public func request(reset:Bool = true) {
+        // APIKeyは各自で入手してください-----------------------------
+        // http://api.gnavi.co.jp/api/manual/restsearch/
+        let APIKey:String = ""
+        //-------------------------------------------------------
+        
+        let params:Parameters = [
+            "keyid":APIKey,
+            "format":"json",
+            "latitude":35.140315,
+            "longitude":136.829216,
+            "range":3
+        ]
+        
+        Alamofire.request("https://api.gnavi.co.jp/RestSearchAPI/20150630/", parameters: params).responseJSON{ response in
+            guard let object = response.result.value else {
+                print("情報が取得できませんでした")
+                return
+            }
+            let json = JSON(object)
+            if json["total_hit_count"] == "0"{
+                print("該当なし")
+            }
+            
+            print(json["total_hit_count"], "件該当しました")
+            
+            if reset { self.delegate.searchResult = [] }
+            
+            for rest in json["rest"] {
+                let data:Dictionary = [
+                    "name" : rest.1["name"],
+                    "imageURL" : rest.1["image_url"]["shop_image1"]
+                ]
+                self.delegate.searchResult.append(data)
+            }
+            print(self.delegate.searchResult)
+        }
+    }
 }
